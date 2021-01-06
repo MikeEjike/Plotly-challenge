d8dcbeb99edf9e37ef5fd1c6b16c607c71af287d
Michael Ejike <mikesoccer12@gmail.com>
2021-01-05 23:46:05 -0500

    commit

---
+++ static/js/app.js
@@ -0,0 +1,21 @@
+const url = 'samples.json'
+
+d3.json(url).then((data) => {
+    console.log(data);
+    let trace = {
+        x: data.samples.map(item => item.otu_ids),
+        y: data.samples.map(item => item.sample_values),
+        type: 'bar'
+    };
+
+    // let layout = {
+    
+    // };
+    
+    let sampleData = [trace];
+
+    Plotly.newPlot("bar", sampleData)
+
+});
+
+
