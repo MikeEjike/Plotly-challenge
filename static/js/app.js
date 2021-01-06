46bef86de8c96c72e85b84d27f2600a88eb90777
Michael Ejike <mikesoccer12@gmail.com>
2021-01-06 00:40:16 -0500

    commit

--- static/js/app.js
+++ static/js/app.js
@@ -2,12 +2,22 @@ const url = 'samples.json'
 
 d3.json(url).then((data) => {
     console.log(data);
+
+    let xValue = data.samples.map(item => item.otu_ids);
+
+    let yValue = data.samples.map(item => item.sample_values);
+    // console.log(xValue)
+    for (let index = 0; index < xValue.length; index++) {
+        const element = xValue[index];
+        
+    }
+    
     let trace = {
-        x: data.samples.map(item => item.otu_ids),
-        y: data.samples.map(item => item.sample_values),
+        x: xValue,
+        y: yValue,
         type: 'bar'
     };
-    console.log(trace)
+    // console.log(trace)
     // let layout = {
     
     // };
